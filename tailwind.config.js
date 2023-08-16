c1a85a89322054f5f59c2e142a2581672e96b305
alda-1995 <aldair@canalla.agency>
2023-08-15 19:37:59 -0600

    make auth mas tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,28 @@ module.exports = {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
+    "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1.5rem',
+          sm: '2rem',
+          lg: '3rem',
+          xl: '3rem',
+          '2xl': '6rem',
+        }
+      },
+      colors: {
+        "primary": "#EB6435",
+        "secondary": "#026C80",
+        "accent": "#003262",
+        "neutral": "#111111",
+        "base-100": "#dddddd",
+      },
+    },
   },
   plugins: [],
 }
